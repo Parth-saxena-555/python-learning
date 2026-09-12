8d1d60b7ede529eefb7e86bae96012034c765a80
parth saxena <parthsaxena.tech@gmail.com>
2026-09-12 15:48:55 +0530

    goo

---
+++ operators2.py
@@ -0,0 +1,13 @@
+# types of operators 
+# arthemetic operators
+a=12
+b=2
+print(a+b)
+# assignment operators
+c=5
+print(c)
+# comparison operators
+print(a==b)
+# logical operators
+print(a>b and a<b)
+print(a>b or a<b)
---
+++ typeofconvertor.py
@@ -0,0 +1,4 @@
+a=23
+print(type(a))
+string=str(a)
+print(type(string))
