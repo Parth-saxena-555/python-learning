59e4c461b7b5e85f4edbd93a85ca92986cccb0ce
parth saxena <parthsaxena.tech@gmail.com>
2026-09-12 16:21:55 +0530

    gyt

---
+++ practiecset1.py
@@ -0,0 +1,33 @@
+# #Q1 write a program  to print sum of two number 
+# a=21
+# b=2
+# print ("sum of two number is",a + b)
+
+
+# # {Q2 Write a program to find reminder}
+# a =23
+# b=2
+# print("reminder z is :",a%b)
+
+
+# Q3 write a program to find type of using input(scanf)
+# a = input("enter your number :")
+# print(type(a))
+
+
+# Q4 write a comparision operator to find out a is greater than b or not
+# where a = 23,b=21
+# a=23
+# b=21
+# print("a is greater than be",a>b)
+
+# Q5 WRITE A PROGRAM TO AVARAGE OF TWO NUMBER INTERED BY USER 
+# a= int (input("enter your number 1: "))
+# b = int (input(" enter your number 2:"))
+# print(" avarage is :", (a+b)/2 )
+
+# Q6/WRITE A PROGRAM TO FIND SQUARE OF NUMBER INTERED BY USER
+
+a = int(input ("enter your number :"))
+print("square is :",a*a)
+
