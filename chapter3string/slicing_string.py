5afa3006a21cd9d5d07703af0c538b20054ae0fd
parth saxena <parthsaxena.tech@gmail.com>
2026-09-12 20:44:48 +0530

    string

---
+++ chapter3string/slicing_string.py
@@ -0,0 +1,9 @@
+# slicesing of name(string)in python
+name = "parthsaxena"
+slicename = name[0:4]
+print(slicename)
+# sliceing meaing cutting string into parts by index format its , 
+# so we use 0 index to start and end upto 4th index,
+#  we can do it like 2sd index to start and 5th index to end
+# eg:
+print(slicename[2:5]) # so its output is rt because r is on 2sd index and ton 5th index
